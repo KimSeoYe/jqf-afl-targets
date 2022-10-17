66233ce4efa52fa537de4011a70300e2aa505d74
KimSeoYe <kimseoye15@gmail.com>
2022-10-17 22:13:02 +0900

    Copy a testsuit for rhino

---
+++ rhino/scripts/classpath.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+JQF_DIR=~/git/jqf
+PROJ_DIR=~/git/jqf-afl-tarets/rhino
+
+# Create classpath
+cp="$JQF_DIR/fuzz/target/classes:$JQF_DIR/fuzz/target/test-classes:$PROJ_DIR/target/classes"
+
+for jar in $PROJ_DIR/target/dependency/*.jar; do
+  cp="$cp:$jar"
+done
+
+echo $cp
