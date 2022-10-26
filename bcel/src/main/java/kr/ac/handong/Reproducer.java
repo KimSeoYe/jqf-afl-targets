8cdc90cc7d00723e4731d607e7d861160a0d799d
KimSeoYe <kimseoye15@gmail.com>
2022-10-26 14:18:40 +0900

    Create a testsuit for bcel

---
+++ bcel/src/main/java/kr/ac/handong/Reproducer.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2017-2018 The Regents of the University of California
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package kr.ac.handong;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import com.pholser.junit.quickcheck.From;
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.JQF;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.ClassFormatException;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.verifier.StatelessVerifierFactory;
+import org.apache.bcel.verifier.VerificationResult;
+import org.apache.bcel.verifier.Verifier;
+import org.junit.Assume;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assume.assumeThat;
+
+public class Reproducer {
+
+    private static void verifyJavaClass(@From(JavaClassGenerator.class) JavaClass javaClass) throws IOException {
+        try {
+            Repository.addClass(javaClass);
+            Verifier verifier = StatelessVerifierFactory.getVerifier(javaClass.getClassName());
+            VerificationResult result;
+            result = verifier.doPass1();
+            assumeThat(result.getMessage(), result.getStatus(), is(VerificationResult.VERIFIED_OK));
+            result = verifier.doPass2();
+            assumeThat(result.getMessage(), result.getStatus(), is(VerificationResult.VERIFIED_OK));
+            for (int i = 0; i < javaClass.getMethods().length; i++) {
+                result = verifier.doPass3a(i);
+                assumeThat(result.getMessage(), result.getStatus(), is(VerificationResult.VERIFIED_OK));
+            }
+        } finally {
+            Repository.clearCache();
+        }
+    }
+
+    private static void testWithInputStream(InputStream inputStream) throws IOException {
+        JavaClass clazz;
+        try {
+            clazz = new ClassParser(inputStream, "Hello.class").parse();
+        } catch (ClassFormatException e) {
+            // ClassFormatException thrown by the parser is just invalid input
+            Assume.assumeNoException(e);
+            return;
+        }
+
+        // Any non-IOException thrown here should be marked a failure
+        // (including ClassFormatException)
+        verifyJavaClass(clazz);
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("usage: java Reproducer [input]");
+            System.exit(1);
+        }
+
+        try {
+            File file = new File(args[0]);
+            InputStream in = new FileInputStream(file);
+            testWithInputStream(in);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
